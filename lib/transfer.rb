3c493c050b7ad98c8afcdecb038ae96455a9bd39
Ynda Jas <yndajas@gmail.com>
2020-07-22 02:50:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,11 +15,12 @@ class Transfer
   end
   
   def execute_transaction
-    if !self.valid?
     if self.valid? && self.status == "pending"
       self.sender.deposit(-amount)
       self.receiver.deposit(amount)
       self.status = "complete"
+    elsif !self.sender.valid?
+      "Transaction rejected. Please check your account balance."
     end
   end
   
