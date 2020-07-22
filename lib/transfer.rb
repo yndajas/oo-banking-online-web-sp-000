3670349533050e2e43aed626f79bfe85fb9102c4
Ynda Jas <yndajas@gmail.com>
2020-07-22 02:51:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,7 @@ class Transfer
   end
   
   def execute_transaction
+    binding.pry
     if self.valid? && self.status == "pending"
       self.sender.deposit(-amount)
       self.receiver.deposit(amount)
