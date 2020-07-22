8177b3b17afdd069ecdfd8cba661a689a874bf61
Ynda Jas <yndajas@gmail.com>
2020-07-22 02:47:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,8 @@ class Transfer
   
   def execute_transaction
     if self.valid?
-      self.sender.deposit(-amount)
-      self.receiver.deposit(amount)   
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount   
     end
   end
   
