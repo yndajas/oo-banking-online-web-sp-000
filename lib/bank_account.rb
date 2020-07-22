a11cfeb56647a76f7f872546af8d41f20b3f7159
Ynda Jas <yndajas@gmail.com>
2020-07-22 02:30:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,6 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
+  attr_accessor :balance, :status
+  attr_reader :name #this should really be an accessor because people can change their names
   
   def initialize(name)
     @name = name
